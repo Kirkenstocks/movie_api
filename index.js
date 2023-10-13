25758da3c9f1e017e0cd47cbcf6446f40aa12c04
Kirk Meyer <kirkmeyer03@gmail.com>
2023-10-12 19:19:18 -0700

    Implemented get request for root directory level

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+
 app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to myFlix! It\'s like IMDB, but worse!');
+});
+
