ffdb5c01126cf12ca3c31c46089297ce6036e5de
Kirk Meyer <kirkmeyer03@gmail.com>
2023-10-27 17:10:02 -0700

    Connected mongoose and mongoDB

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
