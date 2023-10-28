ce2dc4ea2a0877a15820a2c53a7ef613664c7ccc
Kirk Meyer <kirkmeyer03@gmail.com>
2023-10-27 17:14:27 -0700

    Updated function to delete a user

--- index.js
+++ index.js
@@ -153,19 +153,23 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
         res.status(400).send(`${movieTitle} could not be removed from user ${id}'s list of favorite movies.`);
     }
 });
+//     let user = users.find(user => user.id == id);
 
-// Allow users to deregister
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
 
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        users = users.filter(user => user.id != id);
-        res.status(200).send(`User ${id} has been deleted`);
-    } else {
-        res.status(400).send('No such user');
-    }
+// Delete a user by username
+app.delete('/users/:Username', async (req, res) => {
+    await Users.findOneAndRemove({Username: req.params.Username})
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 // Server port setup
