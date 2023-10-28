143851f320e2419ab34f526736c57a92da73bbd7
Kirk Meyer <kirkmeyer03@gmail.com>
2023-10-27 17:12:40 -0700

    Updated function to create new user

--- index.js
+++ index.js
@@ -80,16 +80,29 @@ app.get('/movies/directors/:directorName', (req, res) => {
 });
 
 //Register new users
-app.post('/users', (req, res) => {
-    const newUser = req.body;
-
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser);
-    } else {
-        res.status(400).send('Users need names'); 
-    }
+app.post('/users', async (req, res) => {
+    await Users.findOne({Username: req.body.Username})
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.Username + ' already exists');
+            } else {
+                Users.create({
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday
+                })
+                .then((user) => {res.status(201).json(user)})
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send('Error: ' + error);
+                })
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
 });
 
 // Allow users to update their username
