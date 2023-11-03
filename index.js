fcbfaa73140b290af0f6188ee54c4a209adab283
Kirk Meyer <kirkmeyer03@gmail.com>
2023-11-02 17:42:39 -0700

    Imported express validator

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
+const {check, validationResult} = require('express-validator');
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
