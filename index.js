18d434ad0d6c86f4ada7faf9b4888ee974416096
Kirk Meyer <kirkmeyer03@gmail.com>
2024-02-21 15:46:21 -0800

    Updated get user endpoint

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ app.get('/users', passport.authenticate('jwt', {session: false}), async (req, re
 });
 
 //Return data on one user
-app.get('users/:username', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    await Users.findOne({Username: req.params.username})
+app.get('users/:Username', passport.authenticate('jwt', {session: false}), async (req, res) => {
+    await Users.findOne({Username: req.params.Username})
         .then((user) => {
             res.status(200).json(user);
         })
